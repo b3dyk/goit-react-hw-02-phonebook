fe4deb7f561961be7f11f5749584c5f9cd69d8cb
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-12-26 19:52:49 +0200

    resubmit after comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
 import { Component } from 'react';
-import { Contacts } from './Contacts/Contacts';
+import { customAlphabet } from 'nanoid';
+
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+
+const nanoid = customAlphabet('1234567890id', 4);
 
 export class App extends Component {
   state = {
@@ -12,13 +19,39 @@ export class App extends Component {
     filter: '',
   };
 
-  handleSearch = evt => {
-    const searchValue = evt.target.value.toLowerCase();
+  addUser = data => {
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+
+    this.state.contacts.find(({ name }) => name === data.name)
+      ? alert(`${data.name} already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  handleSearch = ({ target: { value } }) => {
+    const searchValue = value.toLowerCase();
     this.setState({ filter: searchValue });
   };
 
   render() {
     const { contacts, filter } = this.state;
+
+    const visibleContacts = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter)
+    );
+
     return (
       <div
         style={{
@@ -27,14 +60,20 @@ export class App extends Component {
           justifyContent: 'center',
           alignItems: 'center',
           color: '#122236',
+          fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+          marginTop: '16px',
           fontSize: '24px',
         }}
       >
-        <Contacts
-          contacts={contacts}
-          filter={filter}
-          onSearch={this.handleSearch}
-        />
+        <h1 className={css.mainHeading}>Phonebook</h1>
+
+        <ContactForm addUser={this.addUser} />
+
+        <h2 className={css.heading}>Contacts</h2>
+
+        <Filter filter={filter} onSearch={this.handleSearch} />
+
+        <ContactList contacts={visibleContacts} onDelete={this.handleDelete} />
       </div>
     );
   }
---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './ContactItem.module.css';
+
+export const ContactItem = ({ contacts, onDelete }) => {
+  return (
+    <>
+      {contacts.map(({ id, name, number }) => (
+        <li className={css.item} key={id}>
+          <p className={css.text}>
+            {name}: {number}
+          </p>
+          <button
+            className={css.button}
+            type="button"
+            onClick={() => onDelete(id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </>
+  );
+};
+
+ContactItem.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
